fc5dad146b6fdeed1da01b463b49609f9fee242f
lblake <treleven.lloyd@gmail.com>
2023-03-01 15:28:15 +0000

    feat: Implemented MVC model with separation of concerns

---
+++ src/controllers/coffee_controller.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+import * as coffeeService from "../services/coffee_service";
+
+export const getCoffee = async (req: Request<object, object, object, { coffeeName: string | undefined }>, res: Response) => {
+  const { coffeeName } = req.query;
+  const coffee = coffeeService.getCoffee(coffeeName);
+  res.json(coffee).status(200);
+};
+
+export const getCoffeeLover = async (req: Request, res: Response) => {
+  const coffeeLover = coffeeService.getCoffeeLover(); 
+  res.send(coffeeLover);
+}
---
+++ src/services/coffee_service.ts
@@ -0,0 +1,7 @@
+export const getCoffee = (name = 'Latte') => {
+  return {
+    drinkType: 'Coffee',
+    name,
+  };
+};
+export const getCoffeeLover = () => 'I like coffee!';
