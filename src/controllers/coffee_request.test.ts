18ac843022265a7ad1549e14cc6dac5035504c5f
lblake <treleven.lloyd@gmail.com>
2023-03-01 14:48:39 +0000

    feat: Implement new GET request for coffeelover endpoint with parameter and test

--- src/controllers/coffee_request.test.ts
+++ src/controllers/coffee_request.test.ts
@@ -7,4 +7,13 @@ describe("Test home API endpoint request", () => {
     expect(res.statusCode).toEqual(200);
     expect(res.text).toEqual("I like coffee!");
   });
+  test("GET /coffee without a parameter should return correct object", async () => {
+    const res = await request(app).get("/coffee");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({
+      drinkType: "Coffee",
+      name: "Latte",
+    });
+  });
+  
 });
