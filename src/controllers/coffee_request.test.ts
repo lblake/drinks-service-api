77db92f2c1c26adc6a3373831454c5f78799430e
lblake <treleven.lloyd@gmail.com>
2023-03-01 17:37:38 +0000

    feat: Implemented test and endpoint for coffeelover

--- src/controllers/coffee_request.test.ts
+++ src/controllers/coffee_request.test.ts
@@ -1,29 +1,22 @@
 import request from 'supertest';
 import { app } from '../app';
 
-describe('Test home API endpoint request', () => {
-  test('GET should return correct message', async () => {
-    const res = await request(app).get('/coffeelover');
-    expect(res.statusCode).toEqual(200);
-    expect(res.text).toEqual('I like coffee!');
+test('GET /coffee without a parameter should return correct object', async () => {
+  const res = await request(app).get('/coffee');
+  expect(res.statusCode).toEqual(200);
+  expect(res.body).toEqual({
+    drinkType: 'Coffee',
+    name: 'Latte',
   });
-  test('GET /coffee without a parameter should return correct object', async () => {
-    const res = await request(app).get('/coffee');
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toEqual({
-      drinkType: 'Coffee',
-      name: 'Latte',
-    });
-  });
+});
 
-  test('GET /coffee with a different given coffeeName should return correct object', async () => {
-    const res = await request(app)
-      .get('/coffee')
-      .query({ coffeeName: 'Flat White' });
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toEqual({
-      drinkType: 'Coffee',
-      name: 'Flat White',
-    });
+test('GET /coffee with a different given coffeeName should return correct object', async () => {
+  const res = await request(app)
+    .get('/coffee')
+    .query({ coffeeName: 'Flat White' });
+  expect(res.statusCode).toEqual(200);
+  expect(res.body).toEqual({
+    drinkType: 'Coffee',
+    name: 'Flat White',
   });
 });
